4c630532fee04287b3d48a101a6ec64f96dc4153
almaz547 <almaz_xafizov@list.ru>
2019-09-30 16:03:39 +0300

    add Project

--- main.ru.py
+++ main.ru.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Flask
 
-text = requests.get('https://gist.github.com/DanteOnline/f59d99d92202b32515e00155dad3d38c').text
+#text = requests.get('https://gist.github.com/DanteOnline/f59d99d92202b32515e00155dad3d38c').text
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello():
     text = requests.get('https://gist.github.com/DanteOnline/f59d99d92202b32515e00155dad3d38c').text
     return text
-print(text)
+print(hello)
 
 if __name__ == "__main__":
     app.run()

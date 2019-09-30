b8d556e40b46a2b3da7aabeb13cbd2668376f160
almaz547 <almaz_xafizov@list.ru>
2019-09-30 14:42:08 +0300

    add Project

---
+++ main.ru.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask
+
+text = requests.get('https://gist.github.com/DanteOnline/f59d99d92202b32515e00155dad3d38c').text
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    text = requests.get('https://gist.github.com/DanteOnline/f59d99d92202b32515e00155dad3d38c').text
+    return text
+print(text)
+
+if __name__ == "__main__":
+    app.run()
